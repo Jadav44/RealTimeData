849949e7afa7d17b2b92e4d184ddb904cd79eb04
JADAV SAIKRISHNA <122225228+Jadav44@users.noreply.github.com>
2025-03-03 01:50:44 +0530

    updated basic information

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -4,8 +4,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 default_args = {
-    'owner': 'airscholar',
-    'start_date': datetime(2023, 9, 3, 10, 00)
+    'owner': 'jadav',
+    'start_date': datetime(2025, 3, 1, 10, 00)
 }
 
 def get_data():
